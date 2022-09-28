eea39ce508d5dcedbf0e88dcfbd5a32bdb75f5d7
DinikaShenal <dinikashenal@yahoo.com>
2022-09-28 13:51:44 +0300

    Class_06

---
+++ Class_06/first.c
@@ -0,0 +1,14 @@
+// Comment of one row
+// Comment of second row
+
+/*
+Commenting a paragraph or code fragment
+.....
+.....
+*/
+
+int  main()// for row comment there is only // symbols in begin of comment
+{
+ return 100;
+ return 0;
+}
