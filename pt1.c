5a1718d7a829180b1dd06ba4b16a5e660bc4762a
DinikaShenal <dinikashenal@yahoo.com>
2022-10-05 16:15:20 +0300

    Class_08

---
+++ pt1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+ long long int num1,num2,num3;
+ printf("Please enter first number: \n");
+ scanf("%lld",&num1);
+ printf("Please enter second number: \n");
+ scanf("%lld",&num2);
+ num3=num1*num2;
+ printf("The new number is: %lld \n",num3);
+ 
+ return 0;
+}
+
